c95d3256f6e1521db17ba86eb1116ff4b4f6c26f
Mitsikeli1 <silot012@gmail.com>
2016-10-28 15:32:24 +0300

    added change

--- src/testjenkins/TestJenkins.java
+++ src/testjenkins/TestJenkins.java
@@ -16,8 +16,10 @@ public class TestJenkins {
      */
     public static void main(String[] args) {
         
+        int i;
+        for( i=0; i<10; i++){
         System.out.println("Hello World");
-        
+        }
         
     }
     
